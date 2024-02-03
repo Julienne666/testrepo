47959837736e833a011819f241f8f7294e9294cf
Julienne666 <julie6med@gmail.com>
2024-02-03 22:09:09 +0200

    Homework11

---
+++ HomeWork11/HomeWork11/Comparator.cs
@@ -0,0 +1,21 @@
+﻿namespace HomeWork11
+{
+    internal class Comparator : IComparer<int>
+    {
+        public int Compare(int x, int y)
+        {
+            if (x < y)
+            {
+                return -1;
+            }
+            else if (x > y)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
