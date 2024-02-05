cffd51c3657f195ff3e60585290c4ec9aeb909f8
Julienne666 <julie6med@gmail.com>
2024-02-05 23:42:59 +0200

    Homework12

---
+++ HomeWork12/HomeWork12/Models/Contact.cs
@@ -0,0 +1,8 @@
+﻿namespace HomeWork12.Models
+{
+    public abstract class Contact
+    {
+        public abstract string Name { get; }
+        public abstract string Phone { get; }
+    }
+}
---
+++ HomeWork12/HomeWork12/Models/CyrylicContact.cs
@@ -0,0 +1,18 @@
+﻿namespace HomeWork12.Models
+{
+    internal class CyrylicContact : Contact
+    {
+        public CyrylicContact(string name, string phone)
+        {
+            _name = name;
+            _phone = phone;
+        }
+
+        private string _name;
+        private string _phone;
+
+        public override string Name => _name;
+
+        public override string Phone => _phone;
+    }
+}
