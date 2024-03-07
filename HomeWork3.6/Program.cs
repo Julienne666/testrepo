83db1fcaff6b8985ce264a58ba7c6408bbe235e8
julie.medkova <julie6med@gmail.com>
2024-03-08 00:01:28 +0200

    HomeWork3.6

---
+++ HomeWork3.6/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace HomeWork3._6;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        MessageBox messageBox = new MessageBox();
+        messageBox.WindowClosed += (sender, state) => { HandleState(state); };
+
+        Task task = messageBox.Open();
+        task.Wait();
+    }
+
+    private static void HandleState(State state)
+    {
+        if (state == State.Ok)
+        {
+            Console.WriteLine("The operation has been confirmed.");
+        }
+        else
+        {
+            Console.WriteLine("The operation was rejected.");
+        }
+    }
+}
